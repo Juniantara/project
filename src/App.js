dc5a1010dcb4e36d36c187b675deea09a076c821
Juniantara <pandegedeastika37@gmail.com>
2025-07-30 13:38:22 +0800

    feat: create user API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-app.use(express.json()); // Middleware untuk parsing JSON body
 
+app.use(express.json());
 
-// Rute untuk setiap modul
-
+const userRoutes = require('./Routes/userRoutes');
+app.use('/api/users', userRoutes);
 
 // Rute dasar
 app.get('/', (req, res) => {
    res.send('Welcome to the E-commerce API!');
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
